36f3aec2ad61dcf3813c5ed09bd6e9c3dfb8b459
Nikola <nv54194@fer.hr>
2023-11-13 15:32:35 +0100

    Dodani JavaDoc komentari u NatjecanjeController i VirtualnoNatjecanjeController

--- IzvorniKod/src/main/java/hr/fer/progi/looneycodes/BytePit/api/controller/NatjecanjeController.java
+++ IzvorniKod/src/main/java/hr/fer/progi/looneycodes/BytePit/api/controller/NatjecanjeController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Kontroler koji se koristi za pristup metodama vezanim uz Natjecanje entitet.
+ *
+ * @see NatjecanjeService
+ * @see Natjecanje
+ */
 @RestController
 @RequestMapping("/natjecanja")
 public class NatjecanjeController {
@@ -14,17 +20,34 @@ public class NatjecanjeController {
     @Autowired
     private NatjecanjeService natjecanjeService;
 
+    /**
+     * Stvara natjecanje i sprema ga u bazu
+     *
+     * @param natjecanjeDTO objekt s podacima o natjecanju
+     * @return natjecanje
+     */
     @PostMapping("/new")
     public Natjecanje createNatjecanje(@RequestBody CreateNatjecanjeDTO natjecanjeDTO) {
         return natjecanjeService.createNatjecanje(natjecanjeDTO);
     }
 
+    /**
+     * Vraća natjecanje prema jedinstvenom identifikatoru
+     *
+     * @param natjecanjeId identifikator natjecanja
+     * @return natjecanje
+     */
     @GetMapping("/get/{natjecanjeId}")
     public CreateNatjecanjeDTO getNatjecanje(@PathVariable Integer natjecanjeId) {
         Natjecanje natjecanje = natjecanjeService.getNatjecanje(natjecanjeId);
         return new CreateNatjecanjeDTO(natjecanje.getNatjecanjeId(), natjecanje.getNazivNatjecanja(), natjecanje.getPocetakNatjecanja(), natjecanje.getKrajNatjecanja(), natjecanje.getVoditelj().getKorisnikId());
     }
 
+    /**
+     * Vraća sva natjecanja
+     *
+     * @return lista natjecanja
+     */
     @GetMapping("/all")
     public List<CreateNatjecanjeDTO> listAll() {
         return natjecanjeService.listAllNatjecanja().stream().map(natjecanje -> {
@@ -32,7 +55,12 @@ public class NatjecanjeController {
         }).toList();
     }
 
-
+    /**
+     * Vraća natjecanja povezana s zadanim voditeljem
+     *
+     * @param voditeljId
+     * @return lista natjecanja
+     */
     @GetMapping("/get/voditelj/{voditeljId}")
     public List<CreateNatjecanjeDTO> getNatjecanjaByKorisnikId(@PathVariable Integer voditeljId) {
         return natjecanjeService.getNatjecanjaByKorisnikId(voditeljId).stream().map(natjecanje -> {
@@ -40,6 +68,12 @@ public class NatjecanjeController {
         }).toList();
     }
 
+    /**
+     * Metoda koja ažurira postojeće natjecanje
+     *
+     * @param natjecanjeDTO DTO objekt s ažuriranim podacima o natjecanju
+     * @return referenca na azurirano natjecanje
+     */
     @PostMapping("/update")
     public Natjecanje updateNatjecanje(@RequestBody CreateNatjecanjeDTO natjecanjeDTO) {
         return natjecanjeService.updateNatjecanje(natjecanjeDTO);
--- IzvorniKod/src/main/java/hr/fer/progi/looneycodes/BytePit/api/controller/VirtualnoNatjecanjeController.java
+++ IzvorniKod/src/main/java/hr/fer/progi/looneycodes/BytePit/api/controller/VirtualnoNatjecanjeController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
+/**
+ * Kontroler koji se koristi za pristup metodama vezanim uz VirtualnoNatjecanje entitet.
+ *
+ * @see VirtualnoNatjecanjeService
+ * @see VirtualnoNatjecanje
+ */
 @RestController
 @RequestMapping("/virtualnaNatjecanja")
 public class VirtualnoNatjecanjeController {
@@ -16,11 +22,22 @@ public class VirtualnoNatjecanjeController {
     @Autowired
     private VirtualnoNatjecanjeService virtualnoNatjecanjeService;
 
+    /**
+     * Stvara virtualno natjecanje i sprema ga u bazu
+     *
+     * @param virtualnoNatjecanjeDTO objekt s podacima o virtualnom natjecanju
+     * @return virtualno natjecanje
+     */
     @PostMapping("/new")
     public VirtualnoNatjecanje createVirtualnoNatjecanje(@RequestBody VirtualnoNatjecanjeDTO virtualnoNatjecanjeDTO) {
         return virtualnoNatjecanjeService.createVirtualnoNatjecanje(virtualnoNatjecanjeDTO);
     }
 
+    /**
+     * Vraća sva virtualna natjecanja
+     *
+     * @return lista virtualnih natjecanja
+     */
     @GetMapping("/all")
     public List<VirtualnoNatjecanjeDTO> listAll() {
         return virtualnoNatjecanjeService.findAll().stream().map(virtualnoNatjecanje -> {
@@ -28,12 +45,24 @@ public class VirtualnoNatjecanjeController {
         }).toList();
     }
 
+    /**
+     * Vraća virtualno natjecanje prema jedinstvenom identifikatoru
+     *
+     * @param Id identifikator virtualnog natjecanja
+     * @return virtualno natjecanje
+     */
     @GetMapping("/get/{Id}")
     public VirtualnoNatjecanjeDTO getVirtualnoNatjecanje(@PathVariable Integer Id) {
         VirtualnoNatjecanje virtualnoNatjecanje = virtualnoNatjecanjeService.getVirtualnoNatjecanje(Id);
         return new VirtualnoNatjecanjeDTO(virtualnoNatjecanje.getVirtualnoNatjecanjeId(), virtualnoNatjecanje.getOrginalnoNatjecanje().getNatjecanjeId(), virtualnoNatjecanje.getNatjecatelj().getKorisnikId(), virtualnoNatjecanje.getVrijemePocetka());
     }
 
+    /**
+     * Vraća virtualna natjecanja povezana s zadanim natjecateljem
+     *
+     * @param natjecateljId identifikator natjecatelja
+     * @return lista virtualnih natjecanja
+     */
     @GetMapping("/get/natjecatelj/{natjecateljId}")
     public List<VirtualnoNatjecanjeDTO> getVirtualnoNatjecanjeByKorisnikId(@PathVariable Integer natjecateljId) {
         return virtualnoNatjecanjeService.getByKorisnikId(natjecateljId).stream().map(virtualnoNatjecanje -> {
@@ -41,6 +70,12 @@ public class VirtualnoNatjecanjeController {
         }).toList();
     }
 
+    /**
+     * Vraća virtualna natjecanja povezana s zadanim originalnim natjecanjem
+     *
+     * @param origNatId identifikator originalnog natjecanja
+     * @return lista virtualnih natjecanja
+     */
     @GetMapping("/get/origNatjecanje/{origNatId}")
     public List<VirtualnoNatjecanjeDTO> getVirtualnoNatjecanjeByOrigNatId(@PathVariable Integer origNatId) {
         return virtualnoNatjecanjeService.getByOrigNatId(origNatId).stream().map(virtualnoNatjecanje -> {
