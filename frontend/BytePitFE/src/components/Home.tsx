50f64d7f13209e0642c0d223b4c91757b3c2e230
mh53837 <mh53837@fer.hr>
2023-11-14 22:39:42 +0100

    Dodan kalendar ispod kojeg piše koji je datum označen

--- frontend/BytePitFE/src/components/Home.tsx
+++ frontend/BytePitFE/src/components/Home.tsx
@@ -1,28 +1,35 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 interface HomeProps {
     loggedInUser: string | null;
-  }
-  
-  const Home: React.FC<HomeProps> = ({ loggedInUser }) => {
-    return (
-      <div>
-        
-        {loggedInUser ? (
-          <div>
-            <p>pozdrav, {loggedInUser}!</p>
-            {/* Additional content for logged-in users */}
-          </div>
-        ) : (
-          <div>
-            <p>pozdrav, nepoznati korisnik!</p>
-          </div>
-        )}
+}
 
-        <p>*kalendar*</p>
-      </div>
+const Home: React.FC<HomeProps> = ({ loggedInUser }) => {
+    const [date, setDate] = useState<Date>(new Date());
+
+    return (
+        <div>
+            {loggedInUser ? (
+                <div>
+                    <p>Pozdrav, {loggedInUser}!</p>
+                    {/* Additional content for logged-in users */}
+                </div>
+            ) : (
+                <div>
+                    <p>Pozdrav, nepoznati korisnik!</p>
+                </div>
+            )}
+
+            <div className="calendar-container">
+                <Calendar value={date} onChange={(newDate) => setDate(newDate as Date)} />
+            </div>
+            <div className="selected-date-container">
+                Selected date: {date.toDateString()}
+            </div>
+        </div>
     );
-  };
+};
 
 export default Home;
