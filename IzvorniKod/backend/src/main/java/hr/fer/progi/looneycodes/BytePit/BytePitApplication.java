83bca3947ef03a4df4414614563ed17a42b60358
jakovnovak30 <jakovnovak30@gmail.com>
2023-11-15 14:53:50 +0100

    dodan view controller

--- IzvorniKod/backend/src/main/java/hr/fer/progi/looneycodes/BytePit/BytePitApplication.java
+++ IzvorniKod/backend/src/main/java/hr/fer/progi/looneycodes/BytePit/BytePitApplication.java
@@ -5,13 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BytePitApplication {
+public class BytePitApplication implements WebMvcConfigurer {
 
   @Bean
   public PasswordEncoder pswdEcoder(){
     return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/").setViewName("index.html");
   }
 
 	public static void main(String[] args) {
