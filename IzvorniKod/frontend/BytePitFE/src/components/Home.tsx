2f31a2b255143230be8c4a26088b351d8dc2f643
mh53837 <mh53837@fer.hr>
2023-11-15 23:40:34 +0100

    Dodana natjecanja ispod svakog datuma. Dodana označena natjecanja za svaki označeni datum

--- IzvorniKod/frontend/BytePitFE/src/components/Home.tsx
+++ IzvorniKod/frontend/BytePitFE/src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
@@ -6,15 +6,70 @@ interface HomeProps {
     loggedInUser: string | null;
 }
 
+interface Natjecanje {
+    natjecanjeId: number;
+    nazivNatjecanja: string;
+    pocetakNatjecanja: string;
+    krajNatjecanja: string;
+    voditeljId: number;
+}
+
 const Home: React.FC<HomeProps> = ({ loggedInUser }) => {
     const [date, setDate] = useState<Date>(new Date());
+    const [natjecanja, setNatjecanja] = useState<Natjecanje[]>([]);
+    const [oznacenaNatjecanja, setOznacenaNatjecanja] = useState<Natjecanje[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/api/natjecanja/all');
+                const data = await response.json();
+                setNatjecanja(data);
+            } catch (error) {
+                console.error('Greška prilikom dohvaćanja natjecanja:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        const natjecanjaZaDatum = natjecanja.filter(
+            (natjecanje) =>
+                date >= new Date(natjecanje.pocetakNatjecanja) && date <= new Date(natjecanje.krajNatjecanja)
+        );
+
+        setOznacenaNatjecanja(natjecanjaZaDatum);
+    }, [date, natjecanja]);
+
+    const tileContent = ({ date, view }: { date: Date; view: string }) => {
+        if (view === 'month') {
+            const natjecanjaZaDatum = natjecanja.filter(
+                (natjecanje) =>
+                    date >= new Date(natjecanje.pocetakNatjecanja) &&
+                    date <= new Date(natjecanje.krajNatjecanja)
+            );
+
+            return natjecanjaZaDatum.length > 0 ? (
+                <div>
+                    {natjecanjaZaDatum.map((natjecanje) => (
+                        <p key={natjecanje.natjecanjeId} className="naziv-natjecanje">
+                            {natjecanje.nazivNatjecanja}
+                        </p>
+                    ))}
+                </div>
+            ) : null;
+        }
+
+        return null;
+    };
 
     return (
         <div>
             {loggedInUser ? (
                 <div>
                     <p>Pozdrav, {loggedInUser}!</p>
-                    {/* Additional content for logged-in users */}
+                    {/* Dodatni sadržaj za ulogirane korisnike */}
                 </div>
             ) : (
                 <div>
@@ -23,10 +78,18 @@ const Home: React.FC<HomeProps> = ({ loggedInUser }) => {
             )}
 
             <div className="calendar-container">
-                <Calendar value={date} onChange={(newDate) => setDate(newDate as Date)} />
+                <Calendar value={date} onChange={(newDate) => setDate(newDate as Date)} tileContent={tileContent} />
             </div>
             <div className="selected-date-container">
-                Selected date: {date.toDateString()}
+                Označeni datum: {date.toDateString()}
+                <br />
+                {oznacenaNatjecanja.length > 0 ? (
+                    <div>
+                        Označena natjecanja: {oznacenaNatjecanja.map(natjecanje => natjecanje.nazivNatjecanja).join(', ')}
+                    </div>
+                ) : (
+                    <div>Ovog datuma se ne održava natjecanje.</div>
+                )}
             </div>
 
         </div>
@@ -34,4 +97,3 @@ const Home: React.FC<HomeProps> = ({ loggedInUser }) => {
 };
 
 export default Home;
-
