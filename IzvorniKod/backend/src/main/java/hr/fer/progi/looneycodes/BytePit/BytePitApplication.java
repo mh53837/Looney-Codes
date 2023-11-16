7c8796924ea2bc6906abc5be67aa3228f66c1550
Marko Varga <mv54173@fer.hr>
2023-11-16 09:13:20 +0100

    dodana mogućnost refresha

--- IzvorniKod/backend/src/main/java/hr/fer/progi/looneycodes/BytePit/BytePitApplication.java
+++ IzvorniKod/backend/src/main/java/hr/fer/progi/looneycodes/BytePit/BytePitApplication.java
@@ -3,10 +3,14 @@ package hr.fer.progi.looneycodes.BytePit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 
 @SpringBootApplication
 public class BytePitApplication implements WebMvcConfigurer {
@@ -22,6 +26,14 @@ public class BytePitApplication implements WebMvcConfigurer {
     registry.addViewController("/").setViewName("index.html");
   }
 
+  @Bean
+  public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+      return factory -> {
+          ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+          factory.addErrorPages(error404Page);
+      };
+  }
+  
    public static void main(String[] args) {
     SpringApplication.run(BytePitApplication.class, args);
   }
