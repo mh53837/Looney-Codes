12acf0c71a3248c46453b16e30ac6f0f7df95bb4
jakovnovak30 <jakovnovak30@gmail.com>
2024-01-12 19:49:18 +0100

    azurirani application.properties za deployment

--- IzvorniKod/backend/src/main/resources/static/assets/NotFoundPage-233c3394.js
+++
@@ -1,1 +0,0 @@
-import{R as n,S as e,ac as t}from"./index-23e25728.js";class r extends n.Component{render(){return e.jsx("div",{children:e.jsxs("p",{style:{textAlign:"center"},children:[e.jsx("p",{children:e.jsx("b",{children:"404 page not found"})}),e.jsx(t,{to:"/",children:"Go to Home "})]})})}}export{r as default};
--- IzvorniKod/backend/src/main/resources/static/assets/NotFoundPage-2dd9269a.js
+++
@@ -1,1 +0,0 @@
-import{R as n,z as e,X as t}from"./index-d62fd9a5.js";class r extends n.Component{render(){return e.jsx("div",{children:e.jsxs("p",{style:{textAlign:"center"},children:[e.jsx("p",{children:e.jsx("b",{children:"404 page not found"})}),e.jsx(t,{to:"/",children:"Go to Home "})]})})}}export{r as default};
