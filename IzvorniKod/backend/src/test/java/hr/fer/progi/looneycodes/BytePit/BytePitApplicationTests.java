44acf5c7fd58f8ad991356c5733ee514f00767d9
Nikola <nv54194@fer.hr>
2024-01-12 22:01:05 +0100

    dodan selenium test za testiranje login-a

--- IzvorniKod/backend/src/test/java/hr/fer/progi/looneycodes/BytePit/BytePitApplicationTests.java
+++ IzvorniKod/backend/src/test/java/hr/fer/progi/looneycodes/BytePit/BytePitApplicationTests.java
@@ -1,13 +1,43 @@
 package hr.fer.progi.looneycodes.BytePit;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-@SpringBootTest
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class BytePitApplicationTests {
 
+	@BeforeAll
+	public static void setUp() {
+		System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
+	}
 	@Test
-	void contextLoads() {
+	public void test_login_with_valid_credentials() {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://bytepit.onrender.com");
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.cssSelector(".loginDiv button")).click();
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.cssSelector("input[name='korisnickoIme']")).sendKeys("spiderman");
+		driver.findElement(By.cssSelector("input[name='lozinka']")).sendKeys("iLoveMJ");
+		driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(d -> d.findElement(By.cssSelector(".loginDiv button")).getText().equals("odjavi se!"));
+
+		assertEquals("https://bytepit.onrender.com/", driver.getCurrentUrl());
+		driver.quit();
+
+
 	}
 
 }
