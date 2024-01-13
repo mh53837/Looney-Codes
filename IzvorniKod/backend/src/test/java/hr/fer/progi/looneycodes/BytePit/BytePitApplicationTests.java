6d403d8880536277e85301f9b2dca186eef41786
Nikola <nv54194@fer.hr>
2024-01-13 11:17:38 +0100

    dodan test za stvaranje zadatka

--- IzvorniKod/backend/src/test/java/hr/fer/progi/looneycodes/BytePit/BytePitApplicationTests.java
+++ IzvorniKod/backend/src/test/java/hr/fer/progi/looneycodes/BytePit/BytePitApplicationTests.java
@@ -21,7 +21,8 @@ class BytePitApplicationTests {
 	@Test
 	public void test_login_with_valid_credentials() {
 		WebDriver driver = new FirefoxDriver();
-		driver.get("https://bytepit.onrender.com");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.get("http://localhost:8080/");
 
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.findElement(By.cssSelector(".loginDiv button")).click();
@@ -31,13 +32,51 @@ class BytePitApplicationTests {
 		driver.findElement(By.cssSelector("input[name='lozinka']")).sendKeys("iLoveMJ");
 		driver.findElement(By.cssSelector("button[type='submit']")).click();
 
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 		wait.until(d -> d.findElement(By.cssSelector(".loginDiv button")).getText().equals("odjavi se!"));
 
-		assertEquals("https://bytepit.onrender.com/", driver.getCurrentUrl());
+		assertEquals("http://localhost:8080/", driver.getCurrentUrl());
 		driver.quit();
-
 
 	}
 
+	@Test
+	public void test_create_zadatak_success() {
+		WebDriver driver = new FirefoxDriver();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		driver.get("http://localhost:8080/");
+
+		WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".loginDiv button")));
+		loginButton.click();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("korisnickoIme")));
+		driver.findElement(By.name("korisnickoIme")).sendKeys("buttler");
+		driver.findElement(By.name("lozinka")).sendKeys("robin");
+
+		WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
+		submitButton.click();
+
+		WebElement zadaciButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='zadaci']")));
+		zadaciButton.click();
+		zadaciButton.click();
+
+		wait.until(ExpectedConditions.urlContains("/problems/all"));
+		WebElement noviZadatakButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".addBtn")));
+		noviZadatakButton.click();
+
+		// Unos podataka za novi zadatak
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("nazivZadatka")));
+		driver.findElement(By.name("nazivZadatka")).sendKeys("testni zadatak");
+		driver.findElement(By.cssSelector("input[name='tekstZadatka']")).sendKeys("testni tekst");
+		driver.findElement(By.cssSelector("span.ant-select-selection-item")).click();
+		driver.findElements(By.cssSelector(".ant-select-item-option-content")).get(1).click();
+
+		driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+		WebElement spremiPromjeneButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='spremi promjene']")));
+		spremiPromjeneButton.click();
+
+		assertTrue(driver.getCurrentUrl().contains("/user/profile/buttler"));
+		assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[text()='testni zadatak']"))).isDisplayed());
+	}
 }
